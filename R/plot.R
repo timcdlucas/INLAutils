81387c859a040696d145330eedfff7377c9386e9
timcdlucas <timcdlucas@gmail.com>
2016-06-13 17:27:35 +0100

    Add plots of random effects to autoplot.

--- R/plot.R
+++ R/plot.R
@@ -4,7 +4,7 @@
 #' Replicate the plots produced by \link{\code{plot}} using ggplot.
 #'
 #'@param x An inla object
-#'@param which Vector of integers selecting which plots (1 -- 2) are wanted.
+#'@param which Vector of integers selecting which plots (1 -- 4) are wanted.
 #'
 #'@export
 #'
@@ -34,20 +34,35 @@
 #'  p[2] <- p[2] + ggtitle('Hyper parameters')
 #'  p
 
-autoplot.inla <- function(x, which = c(1, 2)){
+autoplot.inla <- function(x, which = 1:3){
 
+  assert_that(is.numeric(which))
+
+  if(!all(which %in% 1:4)){
+    stop('which should only contain integers in 1:3')
+  }
+
+  # Create empty list.
   plots <- list()
-  
+
+  # Check that the plots requested are possible
+  if(length(x$marginals.fixed) == 0){
+    warning('Plot 1 selected in which, but not fixed effects to plot marginals for.')
+    which <- which[which != 1]
+  }
+
 
   # Plot marginals for fixed effects
   if(1 %in% which){
+
+
     # Combine all marginals
     allMarginals <- lapply(seq_len(length(x$marginals.fixed)), 
                       function(p) data.frame(x$marginals.fixed[[p]], var = names(x$marginals.fixed)[p]))
     allMarginals <- do.call(rbind, allMarginals)
 
     # Plot
-    p <- ggplot(allMarginals, aes(x, y)) + 
+    p <- ggplot2::ggplot(allMarginals, aes(x, y)) + 
            facet_wrap('var') +
            geom_line() 
     plots$fixed.marginals <- p
@@ -62,10 +77,44 @@ autoplot.inla <- function(x, which = c(1, 2)){
 
 
     # Plot
-    p <- ggplot(allMarginals, aes(x, y)) + 
+    p <- ggplot2::ggplot(allMarginals, aes(x, y)) + 
            facet_wrap('var') +
            geom_line() 
     plots$hyper.marginals <- p
+  }
+
+  if(3 %in% which){
+    allSummary <- lapply(seq_len(length(x$summary.random)), 
+                      function(p) data.frame(x$summary.random[[p]], var = names(x$summary.random)[p]))
+    allSummary <- do.call(rbind, allSummary)
+
+    # plot
+    p <- ggplot2::ggplot(allSummary, aes(x = as.numeric(ID), y = mean)) +
+           facet_wrap('var', scales = 'free_x') +
+           geom_line() +
+           geom_line(aes(y = X0.025quant), linetype = 2) +
+           geom_line(aes(y = X0.975quant), linetype = 2)
+    plots$random.effects.line <- p
+  }
+
+  if(4 %in% which){
+
+    allMarginals = list()
+    for(i in seq_len(length(x$marginals.random))){
+      allMarginals[[i]] <- lapply(seq_len(length(x$marginals.random[[i]])), 
+                             function(p) data.frame(x$marginals.random[[i]][[p]], 
+                                                    ID = as.character(p), 
+                                                    var = names(x$marginals.random)[i]))
+      allMarginals[[i]] <- do.call(rbind, allMarginals[[i]])
+    }
+    combMarginals <- do.call(rbind, allMarginals)
+    
+
+    # Plot
+    p <- ggplot2::ggplot(combMarginals, aes(ID, y = x)) + 
+           facet_wrap('var', scales = 'free_x') +
+           geom_boxplot(outlier.size = 0.01) 
+    plots$random.effects.boxplots <- p
   }
     
   new('ggmultiplot', plots = plots)
