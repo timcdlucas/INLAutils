14d8c1451af065fc11ed7068b921f7841871b228
Tim Lucas <timcdlucas@gmail.com>
2016-07-27 22:13:16 +0100

    Autoplotmesh now working quite well. *can change most things post hoc. *stroke and things a bit weird *currently defaults to pch=16. Which means fill does nothing... not sure if best.

--- R/autoplotmesh.R
+++ R/autoplotmesh.R
@@ -15,33 +15,56 @@
 #' 
 #' autoplot(mesh)
 #' 
-autoplot.inla.mesh <- function(mesh){
+autoplot.inla.mesh <- function(mesh, col = 'blue', lwd = 0.5, size = 0){
   
-  d <- data.frame(x = mesh$loc[, 1], y = mesh$loc[, 2])
-
+  # extract point data
+  d <- data.frame(x = mesh$loc[, 1], y = mesh$loc[, 2], type = 'evertices')
+  levels(d$type) <- c('evertices', 'adata')
+  d[mesh$idx$loc, 'type'] <- 'adata'
+  # extract lines data. 
+  # mesh$graph$tv column 1, 2, 3 are points in triangles.
+  # Therefore need 1 to 2, 2 to 3 and 3 to 1.
   idx = rbind(mesh$graph$tv[, 1:2, drop = FALSE], 
               mesh$graph$tv[, 2:3, drop = FALSE], 
               mesh$graph$tv[, c(3, 1), drop = FALSE])
-  segments <- data.frame(cbind(mesh$loc[idx[, 1], 1:2], mesh$loc[idx[, 2], 1:2]))
-  names(segments) <- c('x1', 'y1', 'x2', 'y2')
+  segments <- data.frame(mesh$loc[idx[, 1], 1:2], mesh$loc[idx[, 2], 1:2], type = 'bsegments')
+
+  innerouter <- rbind(
+                  data.frame(mesh$loc[mesh$segm$bnd$idx[, 1], 1:2],
+                             mesh$loc[mesh$segm$bnd$idx[, 2], 1:2],
+                             type = 'cbinding'),
+                  data.frame(mesh$loc[mesh$segm$int$idx[, 1], 1:2],
+                             mesh$loc[mesh$segm$int$idx[, 2], 1:2],
+                             type = 'dinternal'))
+  names(segments) <- c('x1', 'y1', 'x2', 'y2', 'type')
+  names(innerouter) <- c('x1', 'y1', 'x2', 'y2', 'type')
+
+  segments <- rbind(segments, innerouter)
 
   
-  ggplot(d, aes(x, y)) +
-    geom_point() +
-    geom_segment(data = segments, aes(x = x1, y = y1, xend = x2, yend = y2))
+  p <- ggplot(data = d, 
+              aes(x, y, 
+                  colour = type, 
+                  size = type, 
+                  shape = type, 
+                  alpha = type, 
+                  linetype = type, 
+                  fill = type)) +
+         geom_segment(data = segments, 
+           aes(x = x1, y = y1, xend = x2, yend = y2)) +
+         geom_point() +
+         theme_minimal() +
+         theme(legend.position = 'none')
+#stroke
+  p <- p +
+         scale_colour_manual(values = c(col, 'darkgrey', 'black', 'black', 'black')) +
+         scale_linetype_manual(values = rep(1, 5)) +
+         scale_size_manual(values = c(size, lwd, 1.3, 1.3, 0)) +
+         scale_shape_manual(values = rep(16, 5)) +
+         scale_alpha_manual(values = rep(1, 5)) +
+         scale_fill_manual(values = rep('red', 5))
 
-
-
-        
+  p
 }
 
-
-#        if (draw.segments) {
-#            if (!is.null(mesh$segm$bnd)) 
-#                lines(mesh$segm$bnd, mesh$loc, lwd = lwd + 1, 
-#                  ...)
-#            if (!is.null(mesh$segm$int)) 
-#                lines(mesh$segm$int, mesh$loc, lwd = lwd + 1, 
-#                  ...)
-#        }
 
