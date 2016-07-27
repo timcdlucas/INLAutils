2892f59b69bbcdd46322dc1565719f0a7560b0cc
Tim Lucas <timcdlucas@gmail.com>
2016-07-27 17:06:16 +0100

    Start autoplot.mesh

---
+++ R/autoplotmesh.R
@@ -0,0 +1,62 @@
+
+
+#' An autoplot (ggplot2) method for INLA mesh objects.
+#'
+#'@param mesh An inla.mesh object
+#'@export
+#'@examples
+#' m = 100
+#' points = matrix(runif(m*2),m,2)
+#' mesh = inla.mesh.create.helper(
+#'   points=points,
+#'   cutoff=0.05,
+#'   offset=c(0.1,0.4),
+#'   max.edge=c(0.05,0.5) )
+#' 
+#' autoplot(mesh)
+#' 
+autoplot.inla.mesh <- function(mesh){
+  
+  d <- data.frame(x = mesh$loc[, 1], y = mesh$loc[, 2])
+
+  # code directly from plot.inla.mesh
+        idx = cbind(mesh$graph$tv[t.sub, c(1:3, 1), drop = FALSE], 
+            NA)
+        x = mesh$loc[t(idx), 1]
+        y = mesh$loc[t(idx), 2]
+
+
+  idx = rbind(mesh$graph$tv[, 1:2, drop = FALSE], 
+              mesh$graph$tv[, 2:3, drop = FALSE], 
+              mesh$graph$tv[, c(3, 1), drop = FALSE])
+  segments <- data.frame(cbind(mesh$loc[idx[, 1], 1:2], mesh$loc[idx[, 2], 1:2]))
+  names(segments) <- c('x1', 'y1', 'x2', 'y2')
+
+  
+  ggplot(d, aes(x, y)) +
+    geom_point() +
+    geom_line(data = segments, aes(x = x1, y = y1, xend = x2, yend = y2))
+
+
+
+        
+  
+        if (draw.edges) {
+            lines(x[1:10], y[1:10], type = "l", col = edge.color, lwd = lwd)
+        }
+
+
+
+}
+
+
+
+
+            idx = unique(as.vector(tv))
+
+            points(mesh$loc[idx, , drop = FALSE], pch = 20, col = vertex.color, 
+                ...)
+            idx = intersect(idx, mesh$idx$loc)
+            points(mesh$loc[idx, , drop = FALSE], pch = 20, col = "blue", 
+                ...)
+
