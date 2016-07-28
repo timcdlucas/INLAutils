48b1f59d15fa1d8c2251f47063c9134e03020d2c
timcdlucas <timcdlucas@gmail.com>
2016-07-28 11:36:21 +0100

    Readd v. basic autoplot test.

--- tests/testthat/test-autoplot.R
+++ tests/testthat/test-autoplot.R
@@ -1,19 +1,17 @@
 context('Basic tests that autoplot produces plots without errors.')
 
 test_that('autoplot does not fail when it should not.', {
-#  library(INLA)
-#  data(Epil)
-#  ##Define the model
-#  formula = y ~ Trt + Age + V4 +
-#           f(Ind, model="iid") + f(rand,model="iid")
-#  result = inla(formula, family="poisson", data = Epil, control.predictor = list(compute = TRUE))
+  library(INLA)
+  data(Epil)
+  ##Define the model
+  formula = y ~ Trt + Age + V4 +
+           f(Ind, model="iid") + f(rand,model="iid")
+  result = inla(formula, family="poisson", data = Epil, control.predictor = list(compute = TRUE))
 
-#  p <- autoplot(result)
-#  p + theme_bw()
+  p <- autoplot(result)
+  p + theme_bw()
 
-#  p[2] <- p[2] + ggtitle('Hyper parameters')
-#  expect_error(p, NA)
-
-expect_true(1 == 1)
+  p[2] <- p[2] + ggtitle('Hyper parameters')
+  expect_error(p, NA)
 
 })
