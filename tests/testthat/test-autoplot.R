a7071a495eed1901f4ba49fa2ad895d7bb946333
timcdlucas <timcdlucas@gmail.com>
2016-07-28 11:47:31 +0100

    Slightly better autoplot tests.

--- tests/testthat/test-autoplot.R
+++ tests/testthat/test-autoplot.R
@@ -8,10 +8,20 @@ test_that('autoplot does not fail when it should not.', {
            f(Ind, model="iid") + f(rand,model="iid")
   result = inla(formula, family="poisson", data = Epil, control.predictor = list(compute = TRUE))
 
-  p <- autoplot(result)
-  p + theme_bw()
-
-  p[2] <- p[2] + ggtitle('Hyper parameters')
+  # Test that figure builds
+  expect_error(p <- autoplot(result), NA)
   expect_error(p, NA)
+
+  # test theme is modifiable
+  expect_error(p2 <- p + theme_bw(), NA)
+
+  expect_error(p2, NA)
+  expect_false(identical(p, p2))
+
+  # Test that you can change a single figure
+  p3 <- p
+  expect_error(p3[2] <- p3[2] + ggtitle('Hyper parameters'), NA)
+  expect_false(identical(p, p3))
+
 
 })
