51a77632a32fa7b65c7b29c52ef4ddf475eec2a5
Tim Lucas <timcdlucas@gmail.com>
2016-08-19 22:04:07 +0100

    Add print and plot methods for inla segment objects.

---
+++ R/plotmethods.R
@@ -0,0 +1,12 @@
+
+#'These are plot methods for a number of inla objects that do not have plot methods in the INLA package
+#'
+#'@param x Object to be printed
+#'@param y not used
+#'@param ... other methods passed to plot.data.frame
+#'@name print.inla.mesh.segment
+#'@export
+
+plot.inla.mesh.segment <- function(x, y, ...){
+  plot(x$loc, type = 'l', ...)
+}
--- R/printmethods.R
+++ R/printmethods.R
@@ -2,10 +2,11 @@
 #'These are print methods for a number of inla objects that do not have print methods in the INLA package
 #'
 #'@param x Object to be printed
+#'@param ... Further arguments passed to or from other methods.
 #'@name print.inla.data.stack
 #'@rdname print.inla.data.stack
 #'@export
-print.inla.data.stack <- function(x){
+print.inla.data.stack <- function(x, ...){
   cat('inla.data.stack object\n')
   cat(paste0('Data: ', x$data$nrow, ' observations. Variables: ', x$data$names))
   cat(paste0('. Effects: ', paste(unlist(x$effects$names), collapse = ', '), '.\n'))
@@ -22,7 +23,7 @@ print.inla.data.stack <- function(x){
 #'@name print.inla.mesh
 #'@rdname print.inla.data.stack
 #'@export
-print.inla.mesh <- function(x){
+print.inla.mesh <- function(x, ...){
   print(summary(x))
 }
 
@@ -31,9 +32,28 @@ print.inla.mesh <- function(x){
 #'@name print.inla.mesh.projector
 #'@rdname print.inla.data.stack
 #'@export
-print.inla.mesh.projector <- function(x){
+print.inla.mesh.projector <- function(x, ...){
   cat('inla.mesh.projector object\n\n')
   cat(paste0('xdim: ', length(x$x), '  ydim: ', length(x$y), '\n'))
   cat(paste0('x range: ', min(x$x), ' to ', max(x$x), '\n'))
   cat(paste0('y range: ', min(x$y), ' to ', max(x$y), '\n'))
 }
+
+
+#'@name print.inla.mesh.segment
+#'@rdname print.inla.data.stack
+#'@export
+print.inla.mesh.segment <- function(x, ...){
+  cat(paste('inla.mesh.segment object with', nrow(x$loc), 'points.\n'))
+  if(is.null(x$grp)){ 
+    grplength <- 1
+  } else {
+    grplength <- length(x$grp)
+  }
+  cat(paste('Number of groups:', grplength, '\n'))
+  extent <- c(range(x$loc[, 1]), range(x$loc[, 2]))
+  cat(paste('xlim:   ', sprintf('%.3f', extent[1]), sprintf('%.3f', extent[2]), '\n'))
+  cat(paste('ylim:   ', sprintf('%.3f', extent[3]), sprintf('%.3f', extent[4]), '\n'))
+}
+
+
