18263e6ef776a9805cd63fb58057902aaeff69d2
Tim Lucas <timcdlucas@gmail.com>
2016-09-06 15:23:42 +0100

    Add first go at adding mesh to plotraster.

--- R/plotraster.R
+++ R/plotraster.R
@@ -6,7 +6,7 @@
 #'@param projector If raster is a matrix, a projector object 
 #'  (from \code{\link[INLA]{inla.mesh.projector}}) is also needed.
 #'@param spatialpolygons A \code{\link[sp]{SpatialPolygonsDataFrame}} object.
-#'@param mesh Not yet implemented. And additional mesh object to plot as well.
+#'@param mesh An inla.mesh object to be plotted. Still rough.
 #'@export
 #'@importFrom reshape2 melt
 #'@importFrom grDevices colorRampPalette
@@ -64,6 +64,11 @@ ggplot_projection_shapefile <- function(raster = NULL,
     stop('If raster is a matrix (i.e. from inla.mesh.project), the projector object is also needed.') 
   }
   
+  # Check mesh is correct class
+  if(!is.null(mesh) & !is(mesh, 'inla.mesh')){
+    stop('mesh musth either be and INLA mesh or NULL')
+  }
+  
   
   # setup colour palette
   cols <- grDevices::colorRampPalette(RColorBrewer::brewer.pal(6, 'Blues'))(1000)
@@ -80,12 +85,50 @@ ggplot_projection_shapefile <- function(raster = NULL,
     
   shape.df <- ggplot2::fortify(spatialpolygons)   
   
-  ggplot() + 
-    ggplot2::geom_raster(data = raster.df, 
-                         aes_string('long', 'lat', fill = 'value')) +
-    ggplot2::geom_path(data = shape.df, 
-                       aes_string('long', 'lat', group = 'group'), 
-                       colour = 'white')
+  plot <- ggplot() + 
+            ggplot2::geom_raster(data = raster.df, 
+                                 aes_string('long', 'lat', fill = 'value')) +
+            ggplot2::geom_path(data = shape.df, 
+                               aes_string('long', 'lat', group = 'group'), 
+                               colour = 'white')
+  
+  if(!is.null(mesh)){
+    d <- data.frame(x = mesh$loc[, 1], y = mesh$loc[, 2], type = 'evertices')
+    levels(d$type) <- c('evertices', 'adata')
+    d[mesh$idx$loc, 'type'] <- 'adata'
+    idx = rbind(mesh$graph$tv[, 1:2, drop = FALSE], 
+                mesh$graph$tv[, 2:3, drop = FALSE], 
+                mesh$graph$tv[, c(3, 1), drop = FALSE])
+    segments <- data.frame(mesh$loc[idx[, 1], 1:2], mesh$loc[idx[, 2], 1:2], type = 'bsegments')
+    
+    innerouter <- rbind(
+      data.frame(mesh$loc[mesh$segm$bnd$idx[, 1], 1:2],
+                 mesh$loc[mesh$segm$bnd$idx[, 2], 1:2],
+                 type = 'cbinding'),
+      data.frame(mesh$loc[mesh$segm$int$idx[, 1], 1:2],
+                 mesh$loc[mesh$segm$int$idx[, 2], 1:2],
+                 type = 'dinternal'))
+    names(segments) <- c('x1', 'y1', 'x2', 'y2', 'type')
+    names(innerouter) <- c('x1', 'y1', 'x2', 'y2', 'type')
+    
+    segments <- rbind(segments, innerouter)
+    
+    # Now add to plot
+    plot <- plot +
+      ggplot2::geom_segment(data = segments, 
+                            ggplot2::aes_string(x = 'x1', y = 'y1', xend = 'x2', yend = 'y2')) +
+      ggplot2::geom_point(data = d, 
+                          ggplot2::aes_string('x', 'y', 
+                                              colour = 'type', 
+                                              size = 'type')) +
+      ggplot2::theme(legend.position = 'none') + 
+      ggplot2::scale_colour_manual(values = c('red', 'red', 'black', 'black', 'black')) +
+      ggplot2::scale_size_manual(values = c(0.8, 1.5, 1.3, 1.3, 0)) 
+    
+  }
+  
+  plot
+  
   
 }
 
