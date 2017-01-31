e49ff268781865f2ace183358a0e674f3f5f9bb4
Tim Lucas <timcdlucas@gmail.com>
2017-01-31 16:08:55 +0000

    Outline of tests for inlasdm.

---
+++ tests/testthat/test-INLAsdm.R
@@ -0,0 +1,26 @@
+context('Tests for INLA sdm functions.')
+
+
+# Can't easily test much beyond error/not error
+
+
+test_that('All INLAsdm options return at least models.', {
+
+  # non spatial
+  
+    # CV
+  
+  
+  # spatial
+  
+    # CV
+  
+  # Step non spatial
+  
+    # CV
+  
+  # step spatial
+    
+    # CV
+
+})
