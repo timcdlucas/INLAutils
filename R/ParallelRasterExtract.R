e152528cebd694872c6bca2fa10712d320694fc3
Tim Lucas <timcdlucas@gmail.com>
2017-02-21 12:46:09 +0000

    Mostly working namespace and examples.

--- R/ParallelRasterExtract.R
+++ R/ParallelRasterExtract.R
@@ -20,29 +20,28 @@
 #'
 #' @export
 #' @examples 
-#' 
+#' library(raster)
 #' # Create raster stack
-#' r <- raster::raster(ncol=36, nrow=18)
-#' r[] <- 1:raster::ncell(r)
+#' r <- raster(ncol=36, nrow=18)
+#' r[] <- 1:ncell(r)
 #' r <- stack(r, r)
 #' 
 #' # Create polygon
 #' cds1 <- rbind(c(-180,-20), c(-160,5), c(-60, 0), c(-160,-60), c(-180,-20))
 #' cds2 <- rbind(c(80,0), c(100,60), c(120,0), c(120,-55), c(80,0))
-#' polys <- raster::spPolygons(cds1, cds2)
+#' polys <- spPolygons(cds1, cds2)
 #' 
 #' #plot(r)
 #' #plot(polys, add=TRUE)
 #' 
 #' # Standard raster::extract
-#' v <- raster::extract(r, polys)
-#' v <- raster::extract(r, polys, fun = mean, df = TRUE)
+#' v <- extract(r, polys)
+#' v <- extract(r, polys, fun = mean, df = TRUE)
 #' 
 #' # Parallel extract
 #' #  Register the cluster first (use doMC on linux)
 #' cl <- snow::makeCluster(2) #change the 2 to your number of CPU cores  
 #' doSNOW::registerDoSNOW(cl)  
-
 #' v2 <- parallelExtract(r, polys, fun = mean)
 #' snow::stopCluster(cl)  
 
