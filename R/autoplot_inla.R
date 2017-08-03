9418340ed50df3df67bac2e06aec3922841fdabf
Tim Lucas <timcdlucas@gmail.com>
2017-08-03 16:19:31 +0100

    Make more examples not run.

--- R/autoplot_inla.R
+++ R/autoplot_inla.R
@@ -143,13 +143,14 @@ autoplot.inla <- function(object, which = c(1:3, 5), priors = FALSE, CI = FALSE,
 #'
 #'
 #'@examples
+#'\dontrun{
 #'  library(INLA)
 #'  data(Epil)
 #'  ##Define the model
 #'  formula = y ~ Trt + Age + V4 +
 #'           f(Ind, model="iid") + f(rand,model="iid")
 #'  result = inla(formula, family="poisson", data = Epil, control.predictor = list(compute = TRUE))
-#'\dontrun{
+#'
 #'  plot_random_effects(result)
 #'  plot_random_effects(result, type = 'boxplot')
 #'  plot_fixed_marginals(result)
